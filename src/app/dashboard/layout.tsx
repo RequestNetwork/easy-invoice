981826b97a9bece1648de9529e0c0bf080ea3947
Bassgeta <kerry.mahne@request.network>
2025-07-18 13:14:32 +0200

    fix: add back create invoice button :')

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,6 +3,8 @@ import { DashboardNavigation } from "@/components/dashboard-navigation";
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 import { getCurrentSession } from "@/server/auth";
+import { PlusCircle } from "lucide-react";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function DashboardLayout({
@@ -20,11 +22,16 @@ export default async function DashboardLayout({
     >
       <Header user={user} />
       <main className="flex-grow flex flex-col w-full max-w-sm sm:max-w-2xl md:max-w-4xl lg:max-w-6xl xl:max-w-[72rem] mx-auto px-4 sm:px-6 lg:px-8 py-8 z-10">
-        <h1 className="mb-2 text-4xl font-bold tracking-tight">Dashboard</h1>
-        <p className="mb-8 text-lg text-muted-foreground">
-          View and manage your invoices, track payments, and monitor business
-          performance
-        </p>
+        <div className="flex justify-between items-center mb-8">
+          <h1 className="mb-2 text-4xl font-bold tracking-tight">Dashboard</h1>
+          <Link
+            href="/invoices/create"
+            className="bg-black hover:bg-zinc-800 text-white transition-colors px-4 py-2 rounded-md flex items-center"
+          >
+            <PlusCircle className="mr-2 h-4 w-4" />
+            Create Invoice
+          </Link>
+        </div>
         <DashboardNavigation />
         {children}
       </main>
