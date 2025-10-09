346579155dff5b77baeecbaa4a235ad4ded5d450
Bassgeta <kerry.mahne@request.network>
2025-10-09 12:24:39 +0200

    build: rework imports according to error logs, skip env validation when building

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 import { validateClientEnv } from "./src/lib/env/client.js";
 import { validateServerEnv } from "./src/lib/env/server.js";
 
-validateServerEnv();
-validateClientEnv();
+if (process.env.SKIP_ENV_VALIDATION !== "true") {
+  validateServerEnv();
+  validateClientEnv();
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
